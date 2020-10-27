b9a6f72944765b8c14b6f6c28a65ad84b1713b9f
Jonathan Kilzi <jonathan.kilzi@888holdings.com>
2020-10-27 11:44:58 +0200

    test #42

--- main.js
+++ main.js
@@ -1,14 +1,23 @@
-function displaySize(target) {
-  return (_) => {
-    const { clientWidth, clientHeight } = target;
-    target.insertAdjacentText("afterbegin", `${clientWidth}x${clientHeight}`);
-  }
+function trimUnits(size) {
+  return size.slice(0, size.length - 2);
+}
+
+function handleResize({ target }) {
+  const { innerWidth, innerHeight } = target;
+  document.title = `${innerWidth}x${innerHeight}`;
+  const vpp = document.querySelector("#virtual-viewport");
+  
+  const { width, height } = getComputedStyle(vpp);
+
+  const scale = Math.min(
+    innerWidth / parseInt(trimUnits(width)),
+    innerHeight / parseInt(trimUnits(height))
+  );
+  vpp.style.transform = `scale(${scale})`;
 }
 
 function main(_) {
-  const vpp = document.querySelector(".virtual-viewport");
-  displaySize(vpp)();
-  window.addEventListener("resize", displaySize(vpp));
+  window.addEventListener("resize", handleResize);
 }
 
-// window.addEventListener("DOMContentLoaded", main);
+window.addEventListener("DOMContentLoaded", main);
