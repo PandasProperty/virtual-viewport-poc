1ec76dcf93662cbe54b67aef7ff625f4720a2f14
Jonathan Kilzi <jonathan.kilzi@888holdings.com>
2020-10-27 16:42:10 +0200

    the best state you can get in portrait

--- main.js
+++ main.js
@@ -2,25 +2,38 @@ function trimUnits(size) {
   return size.slice(0, size.length - 2);
 }
 
-function handleResize({ target }) {
+function updateViewportHeight() {
   const vh = window.innerHeight * 0.01;
-  document.documentElement.style.setProperty('--vh', `${vh}px`);
-  
-  const { innerWidth, innerHeight } = target;
-  document.title = `${innerWidth}x${innerHeight}`;
-  
-  const vpp = document.querySelector("#virtual-viewport");
-  const { width, height } = getComputedStyle(vpp);
+  document.documentElement.style.setProperty("--vh", `${vh}px`);
+}
 
-  const scale = Math.min(
-    innerWidth / parseInt(trimUnits(width)),
-    innerHeight / parseInt(trimUnits(height))
-  );
-  vpp.style.transform = `scale(${scale})`;
+function handleResize(...subjects) {
+  return ({ target }) => {
+    updateViewportHeight();
+    subjects.forEach(s => s.update(target));
+  };
+}
+
+class Subject {
+  #element = null;
+  
+  constructor(element) { this.#element = element; }
+  
+  update({ innerWidth, innerHeight }) {
+    const { width, height } = getComputedStyle(this.#element);
+    const scale = Math.min(
+      innerWidth / parseInt(trimUnits(width)),
+      innerHeight / parseInt(trimUnits(height))
+    );
+    this.#element.style.transform = `scale(${scale})`;
+  }
 }
 
 function main(_) {
-  window.addEventListener("resize", handleResize);
+  const subject = new Subject(document.querySelector("#virtual-viewport"));
+  const handleResizeMemo = handleResize(subject);
+  window.addEventListener("resize", handleResizeMemo);
+  updateViewportHeight();
 }
 
 window.addEventListener("DOMContentLoaded", main);
